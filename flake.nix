f8f6208c72bd27f45a0dc951b33677181d6d8043
King Art <arthur@ischium.net>
2026-06-29 08:24:11 +0100

    reorder flake

--- flake.nix
+++ flake.nix
@@ -12,33 +12,36 @@
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
-      perSystem =
-        { pkgs, ... }:
-        {
-          packages.default = pkgs.runCommand "reality-hs" { } ''
-            mkdir -p $out
-            cp ${./reality.hs} $out/reality.hs
-          '';
+      perSystem = { pkgs, ... }: {
 
-          checks.proof = pkgs.runCommand "proof" {
-            src = ./.;
-            nativeBuildInputs = [ pkgs.lean4 ];
-          } ''
-            export HOME=$(mktemp -d)
-            cp -r "$src" ./work
-            chmod -R u+w ./work
-            cd ./work
-            set -o pipefail
-            lake build --verbose 2>&1 | tee "$out"
-          '';
+        checks.proof =
+          pkgs.runCommand "proof"
+            {
+              src = ./.;
+              nativeBuildInputs = [ pkgs.lean4 ];
+            }
+            ''
+              export HOME=$(mktemp -d)
+              cp -r "$src" ./work
+              chmod -R u+w ./work
+              cd ./work
+              set -o pipefail
+              lake build --verbose 2>&1 | tee "$out"
+            '';
 
-          devShells.default = pkgs.mkShellNoCC {
-            packages = with pkgs; [
-              gnumake
-              lean4
-              python3
-            ];
-          };
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            gnumake
+            lean4
+            python3
+          ];
         };
+
+        packages.default = pkgs.runCommand "reality-hs" { } ''
+          mkdir -p $out
+          cp ${./reality.hs} $out/reality.hs
+        '';
+
+      };
     };
 }
