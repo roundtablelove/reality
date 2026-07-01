4a7e8c1ce72adc05cd0ce7ec02e6862f7e6a5a5e
King Art <arthur@ischium.net>
2026-07-01 14:56:39 +0100

    refactor(flake): replace default.nix with cabal2nix

--- flake.nix
+++ flake.nix
@@ -16,6 +16,19 @@
 
       perSystem = { pkgs, ... }: {
 
+        packages.default = pkgs.haskellPackages.callCabal2nix "reality" ./. { };
+
+        checks.lean =
+          pkgs.runCommand "reality-lean-proofs" { nativeBuildInputs = [ pkgs.lean4 ]; }
+            ''
+              set -eu -o pipefail
+              export HOME=$(mktemp -d)
+              cp -r ${./tests/lean} lean-work
+              chmod -R u+w lean-work
+              cd lean-work
+              lake build --verbose 2>&1 | tee $out
+            '';
+
         devShells.default = pkgs.mkShellNoCC {
           packages = with pkgs; [
             ghc
@@ -23,8 +36,19 @@
           ];
         };
 
-        packages.default = pkgs.callPackage ./default.nix { };
+      };
+
+      flake = {
+
+        overlays.default = _final: prev: {
+          haskellPackages = prev.haskellPackages.extend (
+            hself: _: { reality = hself.callCabal2nix "reality" ./. { }; }
+          );
+        };
+
+        source = ./Reality.hs;
 
       };
+
     };
 }
